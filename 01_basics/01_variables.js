9126d343084031fbe82fbe2c7b9e3ceca9c47d35
Juboraj Sujon <118812363+JuborajSujon@users.noreply.github.com>
2023-12-28 18:11:47 +0000

    learn variable define

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 145675
+let accoutnEmail = "sujon@google.com"
+var accoutnPassword = '12345'
+
+accountCity = "Dhaka"
+
+let accountState;
+
+// accountId = 2
+
+accoutnEmail = "js@gmail.com"
+accoutnPassword = "2323232"
+accountCity = "Rajshahi"
+
+console.log(accoutnEmail);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accoutnEmail, accoutnPassword, accountCity, accountState]);
